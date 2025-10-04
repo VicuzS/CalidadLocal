8228934d8e3d77a091236683dfd6800ed659c5c1
Gerardo <gerarleonce-0703@outlook.es>
2025-10-04 15:31:17 -0500

    Servicio para enviar correos listo, solo falta crear cuenta de la empresa

--- Backend/src/main/java/com/unmsm/scorely/controllers/EmailController.java
+++ Backend/src/main/java/com/unmsm/scorely/controllers/EmailController.java
@@ -1,4 +1,26 @@
 package com.unmsm.scorely.controllers;
 
+import com.unmsm.scorely.dto.CorreoRequest;
+import com.unmsm.scorely.services.EmailService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/correo")
 public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/enviar")
+    public ResponseEntity<String> enviarCorreo(@RequestBody CorreoRequest request) throws MessagingException{
+        emailService.enviarCorreo(
+                request.getPara(),
+                request.getNombre(),
+                request.getCurso(),
+                request.getEnlace()
+        );
+        return ResponseEntity.ok("Correo enviado con éxito bro");
+    }
 }
