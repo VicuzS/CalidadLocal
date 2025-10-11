8ca560182c85855974127971c9a9fe20e12cdc61
Gerardo <gerarleonce-0703@outlook.es>
2025-10-11 00:46:53 -0500

    Agregando los repositorios en interfaces

---
+++ Backend/src/main/java/com/unmsm/scorely/respitory/SeccionRepository.java
@@ -0,0 +1,8 @@
+package com.unmsm.scorely.respitory;
+
+import com.unmsm.scorely.models.Seccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeccionRepository extends JpaRepository<Seccion,Integer> {
+
+}
