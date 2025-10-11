94bebf38409b7a747606d4bfe49bbcddf560661d
Taco Miguel <miguel.taco@unmsm.edu.pe>
2025-10-11 11:52:14 -0500

    boton de eliminar agregado a secciones

--- Frontend/src/componentes/SeccionCard.jsx
+++ Frontend/src/componentes/SeccionCard.jsx
@@ -1,10 +1,12 @@
 import "../styles/SeccionCard.css"
-function SeccionCard({nombre}){
-    return(
-        <div className="seccion-card">
-            <p>{nombre}</p>    
-        </div>
-    );
+
+function SeccionCard({ nombre}) {
+  return (
+    <div className="seccion-card">
+      <p>{nombre}</p>
+      <button>x</button>
+    </div>
+  );
 }
 
 export default SeccionCard;
