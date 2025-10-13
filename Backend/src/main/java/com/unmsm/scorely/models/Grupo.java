46a49f5b56cfc7a4c166983b6d5e23f29e4caa3c
jerson-null <jersonvalquivargas@gmail.com>
2025-10-12 19:21:04 -0500

    Agregando al paquete models los que faltaba ahora ya se puede hacer todo(se supone )

---
+++ Backend/src/main/java/com/unmsm/scorely/models/Grupo.java
@@ -0,0 +1,30 @@
+package com.unmsm.scorely.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Cleanup;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Grupo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_grupo")
+    private Integer idGrupo;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_alumno_seccion", nullable = false)
+    private AlumnoSeccion alumnoSeccion;
+
+    @Column(name = "nombre_grupo", length = 20)
+    private String nombreGrupo;
+
+    @Column(name = "promedio_final")
+    private Integer promedioFinal;
+}
